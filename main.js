e2c01ac9b5145f73b6bd4c3d9534c6dc1f7697b0
juanquiroz09 <juan.quiroz1009@gmail.com>
2022-03-07 13:19:17 -0500

    email

--- main.js
+++ main.js
@@ -2,20 +2,6 @@ document.querySelector('.btn-menu').addEventListener('click', () => {
 	document.querySelector('.navbar').classList.toggle('show');
 });
 
-function sendEmail() {
-	Email.send({
-		Host: "smtp.google.com",
-		Username: "juan.quiroz1009@gmail.com",
-		Password: "C165E7677AC4B2E0817B5C218AE90EE5DCDC",
-		To: 'juan.quiroz1009@gmail.com',
-		From: document.getElementById("email").value,
-		Subject: "New Contact From Enquiry",
-		Body: "Name: " + document.getElementById("name").value +
-			"<br> Email: " + document.getElementById("email").value +
-			"<br> Message: " + document.getElementById("message").value
-	}).then(message => alert("Mensaje Enviado"));
-}
-
 function myFunction() {
 	location.reload();
 }
@@ -49,5 +35,18 @@ function hideWindow2() {
 	document.getElementById("window2").style.display = "none";
 
 	document.getElementById("shadow2").style.display = "none";
+}
 
+function sendEmail() {
+	Email.send({
+		Host: "smtp.google.com",
+		Username: "juan.quiroz1009@gmail.com",
+		Password: "C165E7677AC4B2E0817B5C218AE90EE5DCDC",
+		To: 'juan.quiroz1009@gmail.com',
+		From: document.getElementById("email").value,
+		Subject: "New Contact From Enquiry",
+		Body: "Name: " + document.getElementById("name").value +
+			"<br> Email: " + document.getElementById("email").value +
+			"<br> Message: " + document.getElementById("message").value
+	}).then(message => alert("Mensaje Enviado"));
 }
