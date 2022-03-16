1979ed0a1ca30f1e0d90b65a7265269caaf26b0b
juanquiroz09 <juan.quiroz1009@gmail.com>
2022-03-16 06:19:36 -0500

    modal

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Responsive navBar
 document.querySelector('.btn-menu').addEventListener('click', () => {
 	document.querySelector('.navbar').classList.toggle('show');
 });
@@ -6,37 +7,31 @@ function myFunction() {
 	location.reload();
 }
 
-function displayWindow() {
+//Modals Functions
+var modalBtns = document.querySelectorAll(".modal-open");
 
-	document.getElementById("window").style.display = "block";
+modalBtns.forEach(function(btn){
+  btn.onclick = function() {
+	  var modal = btn.getAttribute("data-modal");
+	  document.getElementById(modal).style.display = "block";
+  };
+});
 
-	document.getElementById("shadow").style.display = "block";
+var closeBtns = document.querySelectorAll(".modal-close");
 
-}
+closeBtns.forEach(function(btn) {
+	btn.onclick = function() {
+		var modal = (btn.closest(".modal").style.display = "none");
+	};
+});
 
-function hideWindow() {
+window.onclick = function(e) {
+	if(e.target.className == "modal"){
+		e.target.style.display = "none";
+	}
+};
 
-	document.getElementById("window").style.display = "none";
-
-	document.getElementById("shadow").style.display = "none";
-
-}
-
-function displayWindow2() {
-
-	document.getElementById("window2").style.display = "block";
-
-	document.getElementById("shadow2").style.display = "block";
-
-}
-
-function hideWindow2() {
-
-	document.getElementById("window2").style.display = "none";
-
-	document.getElementById("shadow2").style.display = "none";
-}
-
+//Functions Contact Form
 function sendEmail() {
 	Email.send({
 		Host: "smtp.google.com",
